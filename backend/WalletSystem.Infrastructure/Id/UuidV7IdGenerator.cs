d4226313ff4abd53546f13fdaa13ad3a8a5f6d89
Reinier Nápoles Martínez <rnapoles86@gmail.com>
2026-09-22 22:08:43 -0500

    feat(wallet.infrastructure): add WalletSystem.Infrastructure layer
    Initializes the infrastructure layer project to handle external concerns.
    This includes the Entity Framework Core DbContext, database migrations,
    repository implementations, and integration with third-party services.

---
+++ backend/WalletSystem.Infrastructure/Id/UuidV7IdGenerator.cs
@@ -0,0 +1,10 @@
+﻿using WalletSystem.Application.Contracts.Services.Id;
+
+namespace WalletSystem.Infrastructure.Id;
+
+public class UuidV7IdGenerator: IIdGenerator
+{
+    public Guid CreateId() => Guid.CreateVersion7();
+    
+    public string AsString() => CreateId().ToString("N"); 
+}
